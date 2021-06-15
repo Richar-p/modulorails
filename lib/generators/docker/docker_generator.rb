1651e68ff2754e8cdab5760f89502325499bdda5
Richar_p <richar_p@modulotech.fr>
2021-06-15 18:04:52 +0200

    feat(Mailcatcher): add mailcatcher to docker compose

--- lib/generators/docker/docker_generator.rb
+++ lib/generators/docker/docker_generator.rb
@@ -13,6 +13,12 @@ class DockerGenerator < Rails::Generators::Base
     template 'docker-compose.prod.yml'
     template 'entrypoints/docker-entrypoint.sh'
     template 'config/database.yml'
+    insert_into_file "config/environments/developmebnt.rb", after: "Rails.application.configure do\n" do
+      "config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.smtp_settings = { address: 'mailcatcher' }"
+    end
+
   rescue StandardError => e
     $stderr.puts("[Modulorails] Error: cannot generate Docker configuration: #{e.message}")
   end
